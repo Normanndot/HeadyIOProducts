0a618dcf8608eb5940b1b159310511b32b37fed9
Norman <thankavijaynorman.d@honeywell.com>
2019-05-30 10:03:05 +0530

    Categories added to DB

--- HeadyIOProducts/Manager/DataManager.swift
+++ HeadyIOProducts/Manager/DataManager.swift
@@ -17,9 +17,89 @@ class DBHandler {
             if key == "categories" {
                 do {
                     let realm = try Realm()
-                    realm.beginWrite()
-                    
-                    let categories = Categories()
+
+                    if let categories = value as? Array<[String: Any]> {
+                        for aCategory in categories {
+                            let category = Category()
+                            realm.beginWrite()
+                            
+                            if let id = aCategory["id"] as? Int {
+                                category.id = id
+                            }
+
+                            if let name = aCategory["name"] as? String {
+                                category.name = name
+                            }
+
+                            if let products = aCategory["products"] as? Array<[String: Any]> {
+                                for aProduct in products {
+                                    let product = Product()
+
+                                    if let id = aProduct["id"] as? Int {
+                                        product.id = id
+                                    }
+                                    
+                                    if let name = aProduct["name"] as? String {
+                                        product.name = name
+                                    }
+
+                                    if let date = aProduct["date_added"] as? String {
+                                        product.dateAdded = date.toDate()
+                                    }
+                                    
+                                    if let variants = aProduct["variants"] as? Array<[String: Any]> {
+                                        for aVariant in variants {
+                                            let variant = Variant()
+
+                                            if let id = aVariant["id"] as? Int {
+                                                variant.id = id
+                                            }
+                                            
+                                            if let color = aVariant["color"] as? String {
+                                                variant.color = color
+                                            }
+
+                                            if let size = aVariant["size"] as? Int {
+                                                variant.size = size
+                                            }
+                                            
+                                            if let price = aVariant["price"] as? Int {
+                                                variant.price = price
+                                            }
+                                            
+                                            product.variants.append(variant)
+                                            realm.add(variant)
+                                        }
+                                    }
+                                    
+                                    let tax = Tax()
+                                    if let aTax = aProduct["tax"] as? [String: Any] {
+                                        if let name = aTax["name"] as? String {
+                                            tax.name = name
+                                        }
+                                        
+                                        if let value = aTax["value"] as? Float {
+                                            tax.value = value
+                                        }
+                                    }
+                                    
+                                    product.tax = tax
+                                    realm.add(product)
+                                    category.products.append(product)
+                                }
+                            }
+
+                            realm.add(category)
+
+                            do {
+                                try realm.commitWrite()
+                                
+                                print(Realm.Configuration.defaultConfiguration.fileURL)
+                            } catch {
+                               print(error.localizedDescription)
+                            }
+                        }
+                    }
                 } catch {
                     print(error.localizedDescription)
                 }
@@ -33,11 +113,11 @@ class DBHandler {
     }
 }
 
-class Categories: Object {
+class Category: Object {
     @objc dynamic var id: Int = 0
-    @objc dynamic var name: String?
-    @objc dynamic var products: [Product]?
-    @objc dynamic var childCategories: [Int]?
+    @objc dynamic var name: String? = nil
+    let products = List<Product>()
+    let childCategories = List<Int>()
     
     override static func primaryKey() -> String? {
         return "id"
@@ -46,12 +126,11 @@ class Categories: Object {
 
 class Product: Object {
     @objc dynamic var id: Int = 0
-    @objc dynamic var name: String?
+    @objc dynamic var name: String? = nil
     @objc dynamic var dateAdded: Date?
-    @objc dynamic var variants: [Variant]?
-    @objc dynamic var tax: Tax?
+    let variants = List<Variant>()
     @objc dynamic var count: Int = 0
-    
+    @objc dynamic var tax: Tax?
     override static func primaryKey() -> String? {
         return "id"
     }
@@ -59,9 +138,9 @@ class Product: Object {
 
 class Variant: Object {
     @objc dynamic var id: Int = 0
-    @objc dynamic var color: String?
+    @objc dynamic var color: String? = nil
     @objc dynamic var size: Int = 0
-    @objc dynamic var price: String?
+    @objc dynamic var price: Int = 0
     
     override static func primaryKey() -> String? {
         return "id"
@@ -69,11 +148,11 @@ class Variant: Object {
 }
 
 class Tax: Object {
-    @objc dynamic var name: String?
-    @objc dynamic var value: String?
+    @objc dynamic var name: String? = nil
+    @objc dynamic var value: Float = 0.0
 }
 
 class Rankings: Object {
-    @objc dynamic var ranking: String?
-    @objc dynamic var products: [Product]?
+    @objc dynamic var ranking: String? = nil
+    let products = List<Product>()
 }
--- HeadyIOProducts/ViewController.swift
+++ HeadyIOProducts/ViewController.swift
@@ -13,10 +13,25 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+
         let manager = NetworkManager(apiHandler: APIHandler(),
                                      parseHandler: ParseHandler(),
                                      dbHandler: DBHandler())
         manager.handle()
+        
     }
 }
 
+extension String {
+    
+    func toDate(withFormat format: String = "yyyy-MM-dd'T'HH:mm:ss.SSSZ")-> Date?{
+        
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = format
+        dateFormatter.locale = Locale(identifier: "en_US_POSIX")
+        let date = dateFormatter.date(from: self)
+        
+        return date
+        
+    }
+}
